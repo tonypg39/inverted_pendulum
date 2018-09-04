3bae83fda522b242ad6d4c71e061798f798099a6
Juan Antonio Piñera García <tonypinera.2015@gmail.com>
2018-09-04 17:19:22 +0000

    Draw the wheels

--- drawings.js
+++ drawings.js
@@ -21,8 +21,7 @@ function draw_floor() {
     ctx.save();
     ctx.fillStyle = '#553333';
     var margin = 3;
-    var fl_h = geo_params.floor_height * geo_params.pix_per_m;
-    console.log(fl_h);
+    var fl_h = geo_params.floor_height * geo_params.pix_per_m;    
     ctx.fillRect(margin, cv_height - fl_h, cv_width - 2 * margin, fl_h - margin)
     ctx.restore();
 }
@@ -33,15 +32,55 @@ function draw_cart() {
     var w_cart = geo_params.w_cart * geo_params.pix_per_m;
     var h_cart = geo_params.h_cart * geo_params.pix_per_m;
     var wheel_rad = sim_params.wheel_rad * geo_params.pix_per_m;
-    var fl_h = geo_params.floor_height * geo_params.pix_per_m
+    var fl_h = geo_params.floor_height * geo_params.pix_per_m;
     ctx.fillStyle = '#121255';
     
     start_x_cart = 400; // x_cv - w_cart / 2;
-    start_y_cart = parseInt(cv_height - 3 - fl_h - 2 * wheel_rad- h_cart);
-    console.log(start_x_cart,start_y_cart,fl_h);
+    start_y_cart = parseInt(cv_height - fl_h - 2 * wheel_rad- h_cart);
     ctx.fillRect(start_x_cart, start_y_cart, w_cart, h_cart);
     //ctx.fillRect(0,0,80,80);
     ctx.restore();
+    draw_wheels();
 }
 
 
+function draw_wheels() {
+    // left wheel
+    x_pos = 400; // state.x * geo_params.pix_per_m;
+    var w_cart = geo_params.w_cart * geo_params.pix_per_m;
+    var wheel_radius = sim_params.wheel_rad * geo_params.pix_per_m;
+    var fl_h = geo_params.floor_height * geo_params.pix_per_m;
+
+    first_x = x_pos + (5/6) *  w_cart;
+    sec_x = x_pos + (1/6) * w_cart;
+    y_w = cv_height - fl_h - wheel_radius;
+    draw_wheel(first_x, y_w, wheel_radius, 50);
+    draw_wheel(sec_x, y_w, wheel_radius, 50);
+}
+
+function draw_wheel(xc,yc,wheel_radius,wheel_angle) {
+    ctx.save();
+    
+    ctx.beginPath();
+    ctx.fillStyle = '#222222'; // Wheel primary color
+    ctx.arc(xc, yc, wheel_radius, (Math.PI / 180) * 0, (Math.PI / 180) * 360, false);
+    ctx.fill();    
+    ctx.closePath();
+
+    w_a =(Math.PI / 180) * wheel_angle;
+    var xp1 = wheel_radius * Math.cos(w_a), yp1 = wheel_radius * Math.sin(w_a);
+    var xp2 = - xp1, yp2 = -yp1;
+
+    ctx.beginPath();
+    ctx.lineWidth = 10;
+    ctx.strokeStyle = "#661111";    // Wheel's stripe color
+    var x1 = xp1 + xc, x2 = xp2 + xc;
+    var y1 = yp1 + yc, y2 = yp2 + yc;
+        
+    ctx.moveTo(x1,y1);
+    ctx.lineTo(x2,y2);
+    ctx.stroke();    
+    ctx.closePath();
+
+    ctx.restore();
+}
--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var cv_height = 500;
 
 var geo_params = {
     w_cart: 2.90, 
-    h_cart: 1.50,  
+    h_cart: 1.00,  
     floor_height: 0.7, 
     pix_per_m : 80
 }
@@ -27,6 +27,6 @@ var state = {
 }
 
 function initialize() {
-    id_draw = setInterval(draw,30);
+    id_draw = setInterval(draw,30);   
 }
 
