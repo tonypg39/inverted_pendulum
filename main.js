cf0bdc54f5976d14818b4b433d6e05fcd206ee91
Juan Antonio Piñera García <tonypinera.2015@gmail.com>
2018-09-04 11:57:55 +0000

    Add the parameter objects

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
+cv_width = 850;
+cv_height = 500;
 
-cv_width = 1050
-cv_height = 500
 
-var floor_height = 60
+geo_params = {
+    w_cart: 60,
+    h_cart: 30,
+    floor_height: 60
+}
+
+sim_params = {
+    L: 50,
+    wheel_rad: 20,
+    m_cart: 10,
+    m_pend: 3,
+    dt: 10
+}
+
+state = {
+    x: 0.0,
+    theta: 0.0,
+    F: 0.0
+}
 
 function draw() {
     cv_env = document.getElementById("sim_environment");
     ctx = cv_env.getContext("2d");
     draw_background();
-    draw_floor();            
+    draw_floor();
 }
 
 function draw_background() {
@@ -23,12 +41,12 @@ function draw_background() {
 
 function draw_floor() {
     ctx.save();
-    ctx.fillStyle = '#553333';    
+    ctx.fillStyle = '#553333';
     var margin = 3
-    ctx.fillRect(margin, cv_height - floor_height, cv_width - 2 * margin, floor_height - margin)
+    ctx.fillRect(margin, cv_height - geo_params.floor_height, cv_width - 2 * margin, geo_params.floor_height - margin)
     ctx.restore();
 }
 
 function draw_(params) {
-    
+
 }
